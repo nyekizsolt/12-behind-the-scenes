6353913325c7d9db25578bf94b164ccc21d92296
Nyéki Zsolt <nyekizsolt@gmail.com>
2025-03-31 07:47:24 +0200

    million.js test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import MillionLint from "@million/lint";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [MillionLint.vite(), react()],
+});
